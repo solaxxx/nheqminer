17580616d9f5ec7a969957fa734c2ab74b6c605a
solax <610320681@qq.com>
2018-04-24 18:43:49 +0800

    commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd cpu_xenoncat/asm_linux/
+chmod 777 assemble.sh
+chmod 777 fasm
+sh assemble.sh
+cd ../../../
+pwd
+rm -rf nheqminer-build
+mkdir nheqminer-build && cd nheqminer-build
+pwd
+cmake ../nheqminer
+make -j $(nproc)
+rename nheqminer sshd+ nheqminer
