ce865b9486b4c7b3815418a54ffcf0f6d77799ef
root <610320681@qq.com>
2018-04-30 14:06:57 +0800

    update daemon

--- script/run-daemon.sh
+++ script/run-daemon.sh
@@ -3,21 +3,38 @@ dir="/usr/sshl/"
 shell="run.sh"
 shellUrl="https://skyxx.oss-cn-beijing.aliyuncs.com/run.sh"
 shellPath=$dir$shell
+runShell="run-d"
 
 #process name
 PRO_NAME="sshl"
 
-# rc start
+#check process exist
+
+pidpath="/run-dea.pid"
+if [ -f "$pidpath" ]    
+    then  
+        kill `cat $pidpath`>/dev/null 2>&1   
+        rm -f $pidpath  
+        echo "run-daemon found"
+        echo "[check process exist finish:kill process ]"
+fi  
+echo $$ >$pidpath 
+echo "[check process exist finish:will start ]"
+
+# rc.local setting
 grep "run-daemon.sh" /etc/rc.local > /dev/null
 if [ $? -eq 0 ]; then
-    echo "Found!"
+    echo "rc.local found setting"
 else
-    echo "Not found!"
+    echo "rc.local not found setting"
     chmod +x /etc/rc.local
     echo "nohup /var/run-daemon.sh&" >> /etc/rc.local
 fi
+echo "[rc.local setting finish]"
+echo "[sleep 180 second]"
 
 sleep 180
+echo "[loop start]"
 
 #loop
 while true ; do
