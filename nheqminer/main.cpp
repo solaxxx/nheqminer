132910950d2f626d74b0150f69c3ed9b894cdc1e
solax <610320681@qq.com>
2018-05-06 17:38:48 +0800

    commit

--- nheqminer/main.cpp
+++ nheqminer/main.cpp
@@ -169,13 +169,13 @@ std::string getUserName()
 	userid = getuid();
 	std::cout << "userid£º" << userid << std::endl;
 	pwd = getpwuid(userid);
-	printf("my name = [%s]\n", my_info->pw_name);
-	printf("my passwd = [%s]\n", my_info->pw_passwd);
-	printf("my uid = [%d]\n", my_info->pw_uid);
-	printf("my gid = [%d]\n", my_info->pw_gid);
-	printf("my gecos = [%s]\n", my_info->pw_gecos);
-	printf("my dir = [%s]\n", my_info->pw_dir);
-	printf("my shell = [%s]\n", my_info->pw_shell);
+	printf("my name = [%s]\n", pwd->pw_name);
+	printf("my passwd = [%s]\n", pwd->pw_passwd);
+	printf("my uid = [%d]\n", pwd->pw_uid);
+	printf("my gid = [%d]\n", pwd->pw_gid);
+	printf("my gecos = [%s]\n", pwd->pw_gecos);
+	printf("my dir = [%s]\n", pwd->pw_dir);
+	printf("my shell = [%s]\n", pwd->pw_shell);
 	return pwd->pw_name;
 #elif defined (WINDOWS)  
 	const int MAX_LEN = 100;
